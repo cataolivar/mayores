069e6fb76a7ffc28301646358debf80cfa5cbbb8
Cata Olivar <cataolivar@gmail.com>
2022-04-12 16:57:44 -0300

    condicional para verificar la edad

---
+++ javascript/main.js
@@ -0,0 +1,9 @@
+edad = prompt ("Es usted mayor de edad?");
+
+edad = (edad);
+
+if (edad >0 && edad < 18) {alert ("Debes ser mayor de edad.");}
+
+else if (edad>17) {alert ("Puedes ingresar.");}
+
+else {}
