52ce43897ad272e6f4d7c9b0590e5640dd988987
Cata Olivar <cataolivar@gmail.com>
2022-04-12 17:49:28 -0300

    new

--- javascript/main.js
+++ javascript/main.js
@@ -7,3 +7,15 @@ if (edad >0 && edad < 18) {alert ("Debes ser mayor de edad.");}
 else if (edad>17) {alert ("Puedes ingresar.");}
 
 else {}
+
+/*
+let edad = prompt ("Ingrese su edad");
+
+if(edad >=18)
+{alert ("Usted puede ingresar.")}
+
+else
+{
+    alert ("Necesitas tener al menos 18 años.")
+}
+*/
